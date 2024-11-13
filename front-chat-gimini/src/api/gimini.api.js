bcfceedbfb155a28580530abefd804eae2c3ac3e
Nilton taco <nilton2005marzo@gmail.com>
2024-11-13 12:53:12 -0500

    add api whit cetifica ssl

--- front-chat-gimini/src/api/gimini.api.js
+++ front-chat-gimini/src/api/gimini.api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 const myApiKey = 
 'AIzaSyBX3SMoSxzqWsAQddIapUn_Pno7krv9gD4';
 export const makeRequest = async(myPrompt) =>{
-    return axios.get(`http://18.209.63.251:8080/prompt`,{
+    return axios.get(`https://api-chat-gpt-nilton.duckdns.org/prompt`,{
         params:{
             prompt : myPrompt,
             apiKey : myApiKey
         }
     });
-}
+};
