b19764f5935eab3daa920ea5b8eda02741c68782
Nilton taco <nilton2005marzo@gmail.com>
2024-11-14 12:42:59 -0500

    add new api key

--- front-chat-gimini/src/api/gimini.api.js
+++ front-chat-gimini/src/api/gimini.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const myApiKey = 
-'AIzaSyBX3SMoSxzqWsAQddIapUn_Pno7krv9gD4';
+'AIzaSyDrWBkVr3JnS1jDFH68yWbUZiZNNWzbGPg';
 export const makeRequest = async(myPrompt) =>{
     return axios.get(`https://api-chat-gpt-nilton.duckdns.org/prompt`,{
         params:{
